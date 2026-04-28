3e70c5da23cd500bd5f91b34ae5e5f0853159f69
xiong <2431242530.com>
2026-04-28 09:12:18 +0800

    update config

--- code/config/config.example.py
+++ code/config/config.example.py
@@ -6,6 +6,7 @@ class Config:
         self.model = ['qwen-plus-2025-07-28',"qwen3.5-omni-plus"]
         self.api_key = ['']
         self.defaultModel = {"baseurl":self.modelBaseUrl[0],"model":self.model[0],"api_key":self.api_key[0]} 
+        self.MultimodalModel = {"baseurl":self.modelBaseUrl[0],"model":self.model[1],"api_key":self.api_key[1]}
         # dataPath
         self.dataPath = os.path.join(os.getcwd(),"code","data")
 
