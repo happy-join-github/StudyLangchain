7ca8ac205193a6e7f592bdd6923eda1ba6d54bba
xiong <2431242530@qq.com>
2026-05-11 23:47:43 +0800

    add tavilyAPIKEY,memory env

--- project/KitchenButler/backEnd/app/core/config.py
+++ project/KitchenButler/backEnd/app/core/config.py
@@ -6,8 +6,10 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
 db_path = os.path.join(os.getcwd(), "data","KitchenButler.db")
+memory_db_path = os.path.join(os.getcwd(),"data","KitchenButlerMemory.db")
+imgspath = os.path.join(os.getcwd(),"data",'images')
 host = os.getenv("host", "127.0.0.1")
 port = int(os.getenv("port", "8080"))
 chatModel = "qwen-plus-2025-07-28"
-predictImgModel = "qwen3.5-omni-plus"
-
+predictImgModel = "qwen-omni-turbo"
+tavily_API_KEY = os.getenv("tailyKey")
